fb76d17efc287f11ccf4ba000af4dd8f48a96f70
subratadhal <subrata.dhal@gmail.com>
2017-04-16 12:07:00 +0530

    react update

---
+++ public/app.jsx
@@ -0,0 +1,4 @@
+ReactDOM.render(
+  <h1> Hello APP!</h1>,
+  document.getElementById('counter')
+);
--- server.js
+++ server.js
@@ -1,3 +1,41 @@
+
+//https://expressjs.com/en/starter/hello-world.html
+// var express = require('express')
+// var app = express();
+//
+// app.get('/', function (req, res) {
+//   res.send('Hello World!')
+// })
+//
+// app.listen(3000, function () {
+//   console.log('Example app listening on port 3000!')
+// })
+
+
+
+//http://www.fullstacktraining.com/articles/how-to-serve-static-files-with-express
+var express = require('express');
+var path = require('path');
+var app = express();
+
+// Define the port to run on
+app.set('port', 3001);
+
+//app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static(__dirname + '/public'));
+
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname + '/public/index.html'));
+// });
+
+// Listen for requests
+var server = app.listen(app.get('port'), function() {
+  var port = server.address().port;
+  console.log('Magic happens on port ' + port);
+});
+
+
 // var express = require('express');
 //
 // //Create our app
@@ -8,14 +46,3 @@
 // app.listen(3000, function(){
 //    console.log('Express server is up on port: 3000');
 // });
-
-var express = require('express')
-var app = express();
-
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
